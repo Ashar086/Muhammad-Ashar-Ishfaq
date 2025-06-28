4911328326136aca52b248dcbd7e19305cec85ec
Muhammad Ashar Ishfaq <161827453+Ashar086@users.noreply.github.com>
2025-06-28 17:52:33 +0500

    Initial commit of my personalized portfolio

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'Samy Rahim';
-export const SITE_DESCRIPTION = 'Samy Rahim, Full StackGenAI Solutions Engineer';
+export const SITE_TITLE = 'Muhammad Ashar Ishfaq';
+export const SITE_DESCRIPTION = 'Muhammad Ashar Ishfaq, Full StackGenAI Solutions Engineer';
 export const SITE_URL = 'https://www.samyrahim.me/';
 export const SITE_IMAGE = 'og.png';
